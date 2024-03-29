80c0131d4833970eb2d692cf1ab102ac11a89d56
varunbehere <varunbehere@gmail.com>
2024-03-29 18:44:12 +0530

    created new project

---
+++ 02_ownReact/main.js
@@ -0,0 +1,9 @@
+const root = document.querySelector('#div')
+const reactElement = {
+    type : 'a',
+    props : {
+        href : 'www.google.com',
+        target : '__blank' 
+    },
+    text : 'Click me'
+}
