cbc27338e67a729459949208922bcb5de194eee2
varunbehere <varunbehere@gmail.com>
2024-04-11 18:25:27 +0530

    learnt tailwind basics and props in react

--- 03_tailwindProps/src/App.jsx
+++ 03_tailwindProps/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400 text-black rounded-xl p-5'>Tailwind Test</h1>
-    
+      <h1 className='bg-green-400 text-black rounded-xl p-5 mb-5'>Tailwind Test</h1>
+      <div className='flex justify-evenly'>
+        <Card name = 'Varun' btnText = 'Visit Profile'/>
+        <Card name = 'Behere' btnText = 'Visit Now'/>
+      </div>
     </>
   )
 }
