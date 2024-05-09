a3be10edfce3d954539d14332aa1d662f00982aa
varunbehere <varunbehere@gmail.com>
2024-05-10 03:33:11 +0530

    Completed Theme Changer

--- Project06-MoreOnContext/src/Context/Theme.js
+++ Project06-MoreOnContext/src/Context/Theme.js
@@ -1,19 +1,13 @@
-import React, { useContext } from "react";
+import { createContext, useContext } from "react";
 
-export const ThemeContext = React.createContext({
-    themeMode : light,
-    lightTheme : ()=>{},
-    darkTheme : ()=>{},
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
 })
 
-export const ThemeContextProvider = ThemeContext.Provider
+export const ThemeProvider = ThemeContext.Provider
 
-export default function useTheme (){
-    return useContext(ThemeContext) 
+export default function useTheme(){
+    return useContext(ThemeContext)
 }
-
-
-
-/*
-In this we have done everything related to Context in the same file and by default we are exporting the useContext
-*/ 
