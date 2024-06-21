3bb48ede356f56e37f9bc6f8c0b29bce9e976297
Varun Behere <varunbehere@gmail.com>
2024-06-21 17:54:07 +0530

    Done with the project

--- Project07-LocalStorage-and-Context/src/Contexts/index.js
+++ Project07-LocalStorage-and-Context/src/Contexts/index.js
@@ -1,2 +1,2 @@
 // import { TodoContext,useTodo, TodoProvider } from "./TodoContext";
-export {TodoContext,useTodo, TodoProvider} from TodoContext
+export {TodoContext,useTodo, TodoProvider} from "./TodoContext"
